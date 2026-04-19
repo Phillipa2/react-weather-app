119b5e986c27cbdfbf31dbb2b1e5f007a64d0343
Phillipa2 <phillipaatieno7@gmail.com>
2026-04-19 13:30:29 -0700

    Added Bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div class="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Phillipa Atieno and is{" "}
       <a href="https://github.com/Phillipa2/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub.</a>
       </footer>
+      </div>
     </div>
   );
 }
