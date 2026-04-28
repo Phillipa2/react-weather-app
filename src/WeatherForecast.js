1a890cd51255e6e71588d3985afd2c6fb1edb53b
Phillipa2 <phillipaatieno7@gmail.com>
2026-04-27 21:46:46 -0700

    Created a new component and sent it data to display the weather forecast for one day

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,32 +1,36 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
 
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
     function handleResponse(response) {
-        console.log(response.data);
+        setForecast(response.data.list);
+        setLoaded(true);
     }
-    console.log(props);
+    
+    if (loaded) {
+        console.log(forecast);
+    return (
+        <div className="WeatherForecast">
+            <div className="row">
+                <div className="col">
+                    <WeatherForecastDay data={forecast[0]}/>
+                </div>
+            </div>
+        </div>
+    );
+    } else {
     let apiKey = "25fb3dce5bdd0825104d2cf9360c6708";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
-    return (
-        <div className="WeatherForecast">
-            <div className="row">
-                <div className="col">
-                    <div className="WeatherForecast-day">Thu</div>
-                    <WeatherIcon code="01d" size={32}/>
-                    <div className="WeatherForecast-temp">
-                        <span className="WeatherForecast-temp-max">19°</span>
-                        <span className="WeatherForecast-temp-min">10°</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+    return "Loading...";
+    }
+    
 }
