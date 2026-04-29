f72f2e709ac0755148f8eb3a0e21b467f735d790
Phillipa2 <phillipaatieno7@gmail.com>
2026-04-28 19:29:23 -0700

    Refactored the code

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
       <Weather defaultCity="Nairobi"/>
       <footer>
         This project was coded by Phillipa Atieno and is{" "}
-      <a href="https://github.com/Phillipa2/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>.
+      <a href="https://github.com/Phillipa2/react-weather-app" target="_blank" rel="noopener noreferrer">open-sourced on GitHub</a>{" "}and{" "}<a href="https://creative-manatee-ed31c1.netlify.app/" target="_blank" rel="noopener noreferrer">hosted on Netlify</a>.
       </footer>
       </div>
     </div>
